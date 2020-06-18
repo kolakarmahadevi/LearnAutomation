7f9f8d4d7fbe111b4a76f08e389f5c4ef55b2116
mahadevi <kolkarmahadevi@gmail.com>
2020-06-18 12:07:28 +0530

    First commit

---
+++ LearnAutomation/src/main/java/TestOne.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class TestOne {
+	
+	@Test
+	public void tc1()
+	{
+		System.out.println("testng firt case1 run");
+	}
+	@Test
+	public void tc2()
+	{
+	 System.out.println("testng second case run ");
+	}
+
+}
+
+
